dac9a31be2128333821466eb4efe3e53ca39661a
LaraA02 <Lara02agrawal@gmail.com>
2024-12-11 10:34:16 -0500

    final

--- tour/tour.js
+++ tour/tour.js
@@ -1,8 +1,9 @@
 console.log("file is working!")
 
-let bgbutton = document.getElementById("button");
-console.log(bgbutton)
-bgbutton.addEventListener("click", function(event){
+let button = document.getElementById("button");
+console.log(button)
+button.addEventListener("click", function(event){
     console.log("clicked!")
     document.body.classList.toggle("active");
 })
+
